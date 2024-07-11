99edddfc48bbcf07b1cb7971243f3eac047ac62a
Jake Loranger <106925875+Jake-loranger@users.noreply.github.com>
2024-07-11 11:58:58 -0400

    AirportWeatherTests
    Create a parent test file

--- AirportWeatherTests/AirportWeatherTests.swift
+++ AirportWeatherTests/AirportWeatherTests.swift
@@ -9,16 +9,10 @@ import XCTest
 
 final class AirportWeatherTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        
-        
+    func testAll() {
+        AWConditionViewModelTests().runTests()
+        AWDetailsVCTests().runTests()
+        AWSearchVCTests().runTests()
+        NetworkManagerTests().runTests()
     }
 }
